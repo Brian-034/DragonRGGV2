eba56f5f504e3f500590810a5bc3a5f60752a4d0
Unknown <brian_cole@talktalk.net>
2018-08-10 23:36:01 +0100

    Animation pack tidy up

---
+++ Assets/_Characters/Player/Player.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using RPG.CameraUI;
+using RPG.Core;
+using RPG.Weapons;
+
+namespace RPG.Characters
+{
+    public class Player : MonoBehaviour, IDamageable
+    {
+
+        [SerializeField] int enemyLayer = 10;
+        [SerializeField] float maxHealthPoints = 100f;
+        [SerializeField] float damagePerHit = 10f;
+        [SerializeField] float minTimeBetweenHits = .5f;
+        [SerializeField] float maxAttackRange = 2f;
+
+        [SerializeField] Weapon weaponInUse;
+        [SerializeField] AnimatorOverrideController animatorOverrideController;
+
+        float currentHealthPoints;
+        CameraRaycaster cameraRaycaster;
+        float lastHitTime = 0f;
+
+        public float healthAsPercentage { get { return currentHealthPoints / maxHealthPoints; } }
+
+        void Start()
+        {
+            RegisterForMouseClick();
+            SetCurentHealth();
+            PutWeaponInHand();
+            OverrideAnimatorController();
+        }
+
+        private void SetCurentHealth()
+        {
+            currentHealthPoints = maxHealthPoints;
+        }
+
+        private void OverrideAnimatorController()
+        {
+            var animator = GetComponent<Animator>();
+            animator.runtimeAnimatorController = animatorOverrideController;
+            animatorOverrideController["DEFAULT ATTACK"] = weaponInUse.GetAttackAnimClip();
+
+         }
+
+        private void PutWeaponInHand()
+        {
+            var weaponPrefab = weaponInUse.GetWeaponPrefab();
+            GameObject dominantHand = RequestDominantHand();
+            var weapon = Instantiate(weaponPrefab, dominantHand.transform);
+            weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
+            weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+        }
+
+        private GameObject RequestDominantHand()
+        {
+            var dominantHands = GetComponentsInChildren<DominantHand>();
+            int numberOfDominantHands = dominantHands.Length;
+            Assert.AreNotEqual(numberOfDominantHands, 0, "No Dominant Hand Found");
+            Assert.IsFalse(numberOfDominantHands > 1, "Multiple Dominant Hands found");
+            return dominantHands[0].gameObject;
+        }
+
+        private void RegisterForMouseClick()
+        {
+            cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+            cameraRaycaster.notifyMouseClickObservers += OnMouseClick;
+        }
+
+        void OnMouseClick(RaycastHit raycastHit, int layerHit)
+        {
+            if (layerHit == enemyLayer)
+            {
+                var enemy = raycastHit.collider.gameObject;
+
+                // Check enemy is in range
+                if ((enemy.transform.position - transform.position).magnitude > maxAttackRange)
+                {
+                    return;
+                }
+
+                var enemyComponent = enemy.GetComponent<Enemy>();
+                if (Time.time - lastHitTime > minTimeBetweenHits)
+                {
+                    enemyComponent.TakeDamage(damagePerHit);
+                    lastHitTime = Time.time;
+                }
+            }
+        }
+
+        public void TakeDamage(float damage)
+        {
+            currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
+        }
+    }
+}
---
+++ Assets/_Weapons/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Weapons
+{
+    [CreateAssetMenu(menuName = "RPG/Weapon")]
+    public class Weapon : ScriptableObject
+    {
+
+        public Transform gripTransform;
+
+        [SerializeField] GameObject weaponPrefab;
+        [SerializeField] AnimationClip attackAnimation;
+
+        public GameObject GetWeaponPrefab()
+        {
+            return weaponPrefab;
+        }
+
+        public AnimationClip GetAttackAnimClip()
+        {
+            RemoveAnimatioEvants(); return attackAnimation;
+        }
+
+        //So that asset packs do not cause crashes
+        private void RemoveAnimatioEvants()
+        {
+            attackAnimation.events = new AnimationEvent[0];
+        }
+    }
+}
